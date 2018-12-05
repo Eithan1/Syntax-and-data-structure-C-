f7a2b4daabbd89607d362f73324751d97019eb54
Eithan1 <44333487+Eithan1@users.noreply.github.com>
2018-12-05 10:31:27 +0800

    抽象类

---
+++ Abstract Class/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Person.h"
+#include "Worker.h"
+#include "Dustman.h"
+
+int main() {
+   //Person person("zhangsan");
+   // Worker worker("zhangsan",20);
+    Dustman dustman("zhangsan",20);
+    return 0;
+}
