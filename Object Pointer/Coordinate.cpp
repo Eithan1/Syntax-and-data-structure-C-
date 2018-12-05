57d93510b96c32eddab8a9cee780f85501fe273f
Eithan1 <44333487+Eithan1@users.noreply.github.com>
2018-12-05 10:53:14 +0800

    对象指针

---
+++ Object Pointer/Coordinate.cpp
@@ -0,0 +1,15 @@
+//
+// Created by 张国梁 on 2018/7/14.
+//
+
+#include "Coordinate.h"
+#include <iostream>;
+using namespace std;
+
+Coordinate::Coordinate() {
+    cout<<"Coordinate"<<endl;
+}
+
+Coordinate::~Coordinate() {
+    cout<<"~Coordinate"<<endl;
+}
---
+++ Object Pointer/Coordinate.h
@@ -0,0 +1,20 @@
+//
+// Created by 张国梁 on 2018/7/14.
+//
+
+#ifndef DUIXIANGZHIZHEN_COORDINATE_H
+#define DUIXIANGZHIZHEN_COORDINATE_H
+
+
+class Coordinate {
+public:
+    Coordinate();
+    ~Coordinate();
+    public:
+    int m_iX;
+    int m_iY;
+
+};
+
+
+#endif //DUIXIANGZHIZHEN_COORDINATE_H
